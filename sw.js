ef09dbd1854d29379468caf205c184dde10cefa9
AUTONATOKE <autonatoke-del@users.noreply.github.com>
2026-09-05 03:45:57 +0300

    feat: add programming and equipment actions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'vtoroy-diagnost-cabin-v4';
-const ASSETS = ['./', './index.html', './app.css?v=4', './app.js?v=4', './manifest.webmanifest'];
+const CACHE = 'vtoroy-diagnost-cabin-v5';
+const ASSETS = ['./', './index.html', './app.css?v=4', './app.js?v=5', './manifest.webmanifest'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener('fetch', (event) => {
